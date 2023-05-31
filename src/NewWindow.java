4bc260f76f99b293018750c37e33baa3f631a67c
Kelly Xia <Kellyx8@nystudents.net>
2023-05-31 09:31:56 -0400

    Added to GUI

--- src/NewWindow.java
+++ src/NewWindow.java
@@ -4,10 +4,32 @@ import java.awt.event.*;
 
 public class NewWindow {
     JFrame frame = new JFrame("Arithmetic Sequence");
-    JLabel label = new JLabel("Welcome");
+    JLabel label = new JLabel("Enter the Starting Number:");
+    JTextField  textField = new JTextField();
+    JLabel label1 = new JLabel("Enter the Operation you want to do (s for subtraction, a for addition):");
+    JTextField textField1 = new JTextField();
+    JLabel label2 = new JLabel("Enter the Common Difference:");
+    JTextField textField2 = new JTextField();
+    JLabel label3 = new JLabel("Enter the number you want to find in the sequence:");
+    JTextField textField3 = new JTextField();
     NewWindow() {
-        label.setBounds(50,0,100,50);
-        label.setFont(new Font(null,Font.PLAIN,25));
+        label.setBounds(0,25,200,50);
+        label.setFont(new Font(null,Font.PLAIN,15));
+        label1.setBounds(0,75,500,50);
+        label1.setFont(new Font(null,Font.PLAIN,15));
+        label2.setBounds(0,125,300,50);
+        label2.setFont(new Font(null,Font.PLAIN,15));
+        label3.setBounds(0,175,500,50);
+        label3.setFont(new Font(null,Font.PLAIN,15));
+
+        textField.setBounds(175,40,100,20);
+
+
+        frame.add(label);
+        frame.add(label1);
+        frame.add(label2);
+        frame.add(label3);
+        frame.add(textField);
 
         frame.add(label);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/NewWindow1.java
+++ src/NewWindow1.java
@@ -4,15 +4,37 @@ import java.awt.event.*;
 
 public class NewWindow1 {
     JFrame frame = new JFrame("Geometric Sequence");
-    JLabel label = new JLabel("Welcome");
+    JLabel label = new JLabel("Enter the Starting Number:");
+    JTextField  textField = new JTextField();
+    JLabel label1 = new JLabel("Enter the Operation you want to do (s for subtraction, a for addition):");
+    JTextField textField1 = new JTextField();
+    JLabel label2 = new JLabel("Enter the Common Difference:");
+    JTextField textField2 = new JTextField();
+    JLabel label3 = new JLabel("Enter the number you want to find in the sequence:");
+    JTextField textField3 = new JTextField();
     NewWindow1() {
-        label.setBounds(50,0,100,50);
-        label.setFont(new Font(null,Font.PLAIN,25));
+        label.setBounds(0,25,100,50);
+        label.setFont(new Font(null,Font.PLAIN,12));
+        label1.setBounds(0,25,100,50);
+        label1.setFont(new Font(null,Font.PLAIN,25));
+        label2.setBounds(0,25,100,50);
+        label2.setFont(new Font(null,Font.PLAIN,25));
+        label3.setBounds(0,25,100,50);
+        label3.setFont(new Font(null,Font.PLAIN,25));
+
+        textField.setBounds(25,25,100,40);
+
 
         frame.add(label);
+        frame.add(label1);
+        frame.add(label2);
+        frame.add(label3);
+        frame.add(textField);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 420);
         frame.setLayout(null);
         frame.setVisible(true);
     }
+
 }
